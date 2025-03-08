d706da74184b9caa8aa931ad20fcd479a0c7a7c3
Nisha Kumari <kumarinish007@gmail.com>
2025-03-08 12:46:26 +0530

    Fix vercel.json and static files issue

--- api/index.js
+++ api/index.js
@@ -3,12 +3,14 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";  
-
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express()
 const port = 3000;
 
